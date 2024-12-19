5064eb2e51dbc8ecbb9a5804c2fbaa57662828d4
thiagohrcosta <thiagohrcosta86@gmail.com>
2024-12-19 17:33:47 -0300

    Complete the default doc

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import { fastify } from 'fastify'
 import { fastifyCors } from '@fastify/cors'
-import { validatorCompiler, serializerCompiler } from 'fastify-type-provider-zod'
+import { validatorCompiler, serializerCompiler, ZodTypeProvider, jsonSchemaTransform } from 'fastify-type-provider-zod'
 import fastifySwagger from '@fastify/swagger'
 import fastifySwaggerUi from '@fastify/swagger-ui'
 import { routes } from './routes'
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
@@ -17,8 +17,9 @@ app.register(fastifySwagger, {
     info: {
       title: 'Typed API',
       version: '1.0.0'
-    }
-  }
+    },
+  },
+  transform: jsonSchemaTransform
 })
 
 app.register(fastifySwaggerUi, {
